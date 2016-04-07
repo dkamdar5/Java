158b20588cacfa7e946ad9b9286cf3d411c542f4
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2016-04-07 13:40:22 -0500

    Fixed Errors

--- CORS/src/main/java/hello/GreetingController.java
+++ CORS/src/main/java/hello/GreetingController.java
@@ -11,20 +11,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Controller
 public class GreetingController {
 
-  private static final String template = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-  @CrossOrigin(origins = "http://localhost:9000")
-  @RequestMapping("/greeting")
-  public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
-    System.out.println("==== in greeting ====");
-    return new Greeting(counter.incrementAndGet(), String.format(template,name));
-  }  
+    @CrossOrigin(origins = "http://localhost:9000")
+    @RequestMapping("/greeting")
+    public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
+        System.out.println("==== in greeting ====");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 
-  @RequestMapping("/greeting-javaconfig")
-  public @ResponseBody Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
-    System.out.println("==== in greeting ====");
-    return new Greeting(counter.incrementAndGet(), String.format(template,name));
-  }
+    @RequestMapping("/greeting-javaconfig")
+    public @ResponseBody Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
+        System.out.println("==== in greeting ====");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 
 }
