eded87a759397fba2e990d3955360548e66b4045
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2016-05-03 21:12:41 -0500

    Added Authentication

--- Spring Security/ui/src/main/java/com/example/UiApplication.java
+++ Spring Security/ui/src/main/java/com/example/UiApplication.java
@@ -1,11 +1,17 @@
 package com.example;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;	
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UiApplication {
 
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
 	@RequestMapping("/resource")
 	public Map<String,Object> home() {
 		Map<String,Object> model = new HashMap<String,Object>();
@@ -24,4 +35,18 @@ public class UiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
 	}
+	
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	      http
+	        .httpBasic()
+	      .and()
+	        .authorizeRequests()
+	          .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+	          .anyRequest().authenticated();
+	    }
+	  }
 }
